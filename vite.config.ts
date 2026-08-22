beb56e396dd33688f9a6b443a8b2acd83c33aab2
iTsukiyomi <meetr4599@gmail.com>
2026-08-22 20:48:44 +0530

    fixed some things

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  base: "/portfolio-react/",
+  base: "/portfolio/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
